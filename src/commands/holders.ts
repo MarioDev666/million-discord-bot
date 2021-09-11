0544ee9c2f85ee12fccdea73ca0dedb250fa8789
MarioDev666 <88241341+MarioDev666@users.noreply.github.com>
2021-09-11 07:55:16 +0200

    Update holders.ts

--- src/commands/holders.ts
+++ src/commands/holders.ts
@@ -1,6 +1,6 @@
-import { SlashCommand} from 'slash-create';
+import { CommandContext, SlashCommand} from 'slash-create';
 import {MillionStatsService} from '../services/MillionStatsService';
-import * as Discord from 'discord.js';
+import * as Discord from'discord.js';
 
 module.exports = class HelloCommand extends SlashCommand {
   constructor(creator) {
@@ -14,51 +14,9 @@ module.exports = class HelloCommand extends SlashCommand {
     this.filePath = __filename;
   }
 
-  async run(ctx) {
+  async run(ctx: CommandContext) {
     let exampleEmbed;
     try {
-      exampleEmbed = new Discord.MessageEmbed()
-            .setColor('#C51162')//Pink50 (A700)
-            .addField(
-              `Getting Holder's Count...`, `\u200B`,
-              false
-            );
-      await ctx.send({embeds: [exampleEmbed], ephemeral: true}).then(async (msg) =>{
-        //your code here! msg.edit will work here.
-        const {data, hasError, error} = await MillionStatsService.getHolders();
-      if (hasError) throw error;
-
-      exampleEmbed = new Discord.MessageEmbed()
-            .setColor('#C51162')//Pink50 (A700)
-            .addField(
-              `Total MM Hodlers <:pepeholdmm:861835461458657331>`, 
-              `${data.totalHodlers}`, 
-              false
-            )
-            .addField(
-              `Uniswap`,
-              data.uniswap,
-              false
-            )
-            .addField(
-              'BSC',
-              data.bsc,
-              false
-            )
-            .addField(
-              'Polygon',
-              data.polygon,
-              false
-            )
-            .addField(
-              'Solana',
-              data.solana,
-              false
-            );
-        await msg.edit({embeds: [exampleEmbed], ephemeral: true})
-    })
-
-    /*
       const {data, hasError, error} = await MillionStatsService.getHolders();
       if (hasError) throw error;
 
@@ -70,11 +28,6 @@ module.exports = class HelloCommand extends SlashCommand {
               false
             )
             .addField(
-              `Uniswap`,
-              data.uniswap,
-              false
-            )
-            .addField(
               'BSC',
               data.bsc,
               false
@@ -88,17 +41,20 @@ module.exports = class HelloCommand extends SlashCommand {
               'Solana',
               data.solana,
               false
+            )
+            .addField(
+              `Uniswap`,
+              data.uniswap,
+              false
             );
-            
-            await msg.edit({embeds: [exampleEmbed], ephemeral: true})
-        //await ctx.send({embeds: [exampleEmbed], ephemeral: true});
-        */
+          
+        return await ctx.send({embeds: [exampleEmbed], ephemeral: true});
     } catch (error) {
       console.log('"holders" command error: \n', error);
       exampleEmbed = new Discord.MessageEmbed()
       .setColor('#C51162')//Pink50 (A700)
       .addField(`Something went wrong`, `try again a bit later.`)
-      await ctx.send({embeds: [exampleEmbed], ephemeral: true});
+      return await ctx.send({embeds: [exampleEmbed], ephemeral: true});
     }
   }
 };

d9f9533ccab22da200c95f17d85fba6bea96eeb4
MarioDev666 <88241341+MarioDev666@users.noreply.github.com>
2021-10-04 23:39:31 +0200

    ...

--- src/channel-IDs/devChannelIds.ts
+++ src/channel-IDs/devChannelIds.ts
@@ -1,3 +1,4 @@
+/*
 export const devChannelIds = {
   lounge: '882029588937572427',
   suggestion_box: '882029588937572429',
@@ -10,4 +11,19 @@ export const devChannelIds = {
   telegramCount: '882029588585259037',
   youTubePromotion: '885463886487252993',
   botCommand_DevChannel: '885463886487252993'
-}//Mario's IDs
+}
+*/
+
+export const devChannelIds = {
+  lounge: '882029588937572427',
+  suggestion_box: '882029588937572429',
+  botCommandsChannel: '894351904568651798',
+  twitterStats: '894351903905964112',
+  redditStats: '894351903536853011',
+  emailSubsChannel: '894351903905964113',
+  holdersChannel: '894351903536853008',
+  price: '894351903536853007',
+  telegramCount: '894351903536853009',
+  youTubePromotion: '885463886487252993',
+  botCommand_DevChannel: '894351902693818396'
+}
--- src/role-IDs/prod.ts
+++ src/role-IDs/prod.ts
@@ -1,6 +1,6 @@
 export const prodRoleIds = {
-  leadAdmin: '870429095941517332',
-  admins: '870411279108571216',
-  leadDev: '870435957969657886',
-  leadAmbassador: '870501520796418059'
+  leadAdmin: '894351901112565809',
+  admins: '894351901112565808',
+  leadDev: '894351901112565809',
+  leadAmbassador: '894351901112565809'
 }
